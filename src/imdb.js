7344fd260a774c4d5774d723f4634d7c3b227ffe
johackim <contact@johackim.com>
2017-06-22 16:25:38 +0200

    Add inquirer prompts

---
+++ src/imdb.js
@@ -0,0 +1,14 @@
+export const get = search => [
+    {
+        name: 'Matrix',
+        value: 'tt2234239',
+    },
+    {
+        name: 'Matrix Reloaded',
+        value: 'tt9329234',
+    },
+    {
+        name: 'Matrix Revolutions',
+        value: 'tt3292394',
+    },
+];
