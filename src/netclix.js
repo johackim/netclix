9ba0af37583a6bbc4ce188a227b61438c3800100
stoneo <stoneo@users.noreply.github.com>
2017-06-22 19:17:20 +0200

    Refactoring

--- src/netclix.js
+++ src/netclix.js
@@ -1,72 +1,49 @@
 #!/usr/bin/env node
 
 import 'babel-polyfill';
-import phantom from 'phantom';
 import ora from 'ora';
-import debug from 'debug';
 import inquirer from 'inquirer';
 import imdb from './imdb';
-
-const log = debug('all');
+import vodlocker from './vodlocker';
 
 (async () => {
     const search = await inquirer.prompt({
         type: 'input',
         name: 'search',
         message: 'What movie are you looking for?',
+        validate: x => !!x || 'Please enter a valid search.',
     });
+
+    let choices;
+
+    try {
+        const spinner = ora('Searching movies...').start();
+        choices = await imdb(search.search);
+        spinner.stop();
+
+        if (!choices.length) {
+            spinner.fail(`Couldn't find any results matching ${search.search}`);
+            process.exit(0);
+        }
+    } catch (e) {
+        console.error('Unknow error, retry...');
+        process.exit(0);
+    }
 
     const movie = await inquirer.prompt({
         type: 'list',
         name: 'id',
-        message: 'Choose one of the items:',
-        choices: await imdb(search.search),
+        message: 'Choose one of the movies:',
+        choices,
     });
 
-    const id = movie.id;
-
-    let attempt = 0;
     const spinner = ora('Searching streaming link').start();
 
-    const renderPage = async (url) => {
-        if (attempt === 5) {
-            spinner.fail('No link');
-            spinner.stop();
-            process.exit(0);
-        }
-
-        const instance = await phantom.create();
-        const page = await instance.createPage();
-
-        await page.open(url);
-
-        const embed = await page.evaluate(() => {
-            if (document.getElementById('player_frame')) {
-                return document.getElementById('player_frame').innerHTML.replace(/&quot;/g, '"');
-            }
-
-            return null;
-        });
-
-        if (!embed) {
-            attempt += 1;
-            log(`Attempt ${attempt}`);
-            await instance.exit();
-            return await renderPage(url); // eslint-disable-line
-        }
-
-        await instance.exit();
-        return embed;
-    };
-
-    const url = `http://vodlocker.to/embed?i=${id}`;
-    const embed = await renderPage(url);
-    const link = embed.match(/(((http[s]?|ftp):\/)?\/?([^:/\s]+)((\/\w+)*\/)([\w\-.]+[^#?\s]+)(.*)?(#[\w-]+)?)",/)[1];
-
-    if (link) {
+    try {
+        const link = await vodlocker(movie.id);
         spinner.succeed(`Your streaming link is here: ${link}`);
-    } else {
-        spinner.fail('No link');
+    } catch (e) {
+        spinner.fail(e.message);
     }
 
     spinner.stop();
