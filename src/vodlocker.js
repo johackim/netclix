2f4a7e1491675b56dac1cd76586f9c725ea75b56
ThatCynicalX <koolboync@gmail.com>
2017-08-02 10:30:02 -0700

    Implement TV-Series support into netclix.js
    Add TV support to imdb.js
    
    Add TV support to vodlocker.js

--- src/vodlocker.js
+++ src/vodlocker.js
@@ -7,28 +7,45 @@ const log = debug('all');
 let attempt = 0;
 const maxAttempts = 5;
 
-const vodlocker = async (id) => {
+let embed;
+
+const vodlocker = async (id, season, episode) => {
     if (attempt >= maxAttempts) {
         throw new Error(`No streaming link, try to test on http://vodlocker.to/embed?i=${id}`);
     }
-
-    const embed = await new Horseman(phantomjs).open(`http://vodlocker.to/embed?i=${id}`)
+    if (season !== 'undefined') {
+        if (episode !== 'undefined') {
+            embed = await new Horseman(phantomjs).open(`http://vodlocker.to/embed?i=${id}&season=${season}&episode=${episode}`)
+           .evaluate(() => {
+               if (document.getElementById('player_frame')) {
+                   return document.getElementById('player_frame').innerHTML;
+               }
+               return null;
+           })
+           .close();
+            if (!embed) {
+                attempt += 1;
+                log(`Attempt ${attempt} ${id} ${season} ${episode}`);
+                await sleep(1000);
+                return vodlocker(id, season, episode);
+            }
+        }
+    } else {
+        embed = await new Horseman(phantomjs).open(`http://vodlocker.to/embed?i=${id}`)
         .evaluate(() => {
             if (document.getElementById('player_frame')) {
                 return document.getElementById('player_frame').innerHTML;
             }
-
             return null;
         })
-    .close();
-
-    if (!embed) {
-        attempt += 1;
-        log(`Attempt ${attempt} ${id}`);
-        await sleep(1000);
-        return vodlocker(id);
+        .close();
+        if (!embed) {
+            attempt += 1;
+            log(`Attempt ${attempt} ${id}`);
+            await sleep(1000);
+            return vodlocker(id);
+        }
     }
-
     return embed.match(/(((http[s]?|ftp):\/)?\/?([^:/\s]+)((\/\w+)*\/)([\w\-.]+[^#?\s]+)(.*)?(#[\w-]+)?)&quot;,/)[1];
 };
 
