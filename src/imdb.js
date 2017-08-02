923534bfa4b7de6a44fad78c83f916d67cbc00a5
ThatCynicalX <koolboync@gmail.com>
2017-08-02 10:31:11 -0700

    Add TV support to imdb.js

--- src/imdb.js
+++ src/imdb.js
@@ -1,12 +1,12 @@
 import phantomjs from 'phantomjs-prebuilt';
 import Horseman from 'node-horseman';
 
-export default async search => new Horseman(phantomjs).open(`http://www.imdb.com/find?q=${search}&s=tt&ttype=ft`)
+export default async search => new Horseman(phantomjs).open(`http://www.imdb.com/find?q=${search}&s=tt&ttype=fn_tt`)
     .evaluate(() => {
-        const movies = [].slice.call(document.querySelectorAll('.result_text a')).slice(0, 10);
-        return movies.map(movie => ({
-            name: movie.text,
-            value: movie.href.match(/tt[0-9]+/)[0],
+        const titles = [].slice.call(document.querySelectorAll('.result_text a')).slice(0, 10);
+        return titles.map(title => ({
+            name: title.text,
+            value: title.href.match(/tt[0-9]+/)[0],
         }));
     })
 .close();
