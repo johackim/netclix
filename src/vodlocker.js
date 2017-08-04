de1a693e58650105bb8b79aab6b011027bd638a7
ThatCynicalX <koolboync@gmail.com>
2017-08-04 14:35:39 -0700

    Fixed more syntax errors

--- src/vodlocker.js
+++ src/vodlocker.js
@@ -17,30 +17,30 @@ const vodlocker = async (name, season, episode) => {
     }
 
     if (!!season && !!episode) {
-        const embed = await new Horseman(phantomjs).open(`https://vodlocker.to/embed?referrer=link&t=${nameEncoded}&season=${season}&episode=${episode}`)
+        embed = await new Horseman(phantomjs).open(`https://vodlocker.to/embed?referrer=link&t=${nameEncoded}&season=${season}&episode=${episode}`)
         .evaluate(() => {
             if (document.getElementById('player_frame')) {
                 return document.getElementById('player_frame').innerHTML;
             }
 
             return null;
-        })
-    	.close();
+	})
+	.close();
 
-    	if (!embed) {
-        	attempt += 1;
-        	log(`Attempt ${attempt} ${name} ${season} ${episode}`);
-        	await sleep(1000);
-        	return vodlocker(name, season, episode);
-       		} 
-    } else { 
-	embed = await new Horseman(phantomjs).open(`http://vodlocker.to/embed?i=${id}`) .evaluate(() => {
-            if (document.getElementById('player_frame')) {
-                return document.getElementById('player_frame').innerHTML;
-            }
-
-            return null;
-        })
+		if (!embed) {
+			attempt += 1;
+			log(`Attempt ${attempt} ${name} ${season} ${episode}`);
+			await sleep(1000);
+			return vodlocker(name, season, episode);
+		}
+	} else {
+		embed = await new Horseman(phantomjs).open(`http://vodlocker.to/embed?i=${name}`) .evaluate(() => {
+			if (document.getElementById('player_frame')) {
+				return document.getElementById('player_frame').innerHTML;
+			}
+	
+			return null;
+		})
         .close();
         if (!embed) {
             attempt += 1;
